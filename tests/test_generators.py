d163f67b4522aa31fde556a703069012566babd9
Krivova Yuliya <charrly13swan@gmail.com>
2025-03-24 01:00:19 +0300

    add параметризация in test_generators.py, UpDate htmlcov

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -57,59 +57,87 @@ def transactions() -> List[Dict]:
     ]
 
 
-@pytest.fixture
-def usd_transactions() -> List[Dict]:
-    return [
-        {
-            "id": 939719570,
-            "state": "EXECUTED",
-            "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод организации",
-            "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702",
-        },
-        {
-            "id": 142264268,
-            "state": "EXECUTED",
-            "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод со счета на счет",
-            "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188",
-        },
-        {
-            "id": 895315941,
-            "state": "EXECUTED",
-            "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
-            "description": "Перевод с карты на карту",
-            "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229",
-        },
-    ]
+# Тесты для filter_by_currency
+@pytest.mark.parametrize(
+    "code, usd_transaction",
+    [
+        (
+            "USD",
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+                {
+                    "id": 142264268,
+                    "state": "EXECUTED",
+                    "date": "2019-04-04T23:20:05.206878",
+                    "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 19708645243227258542",
+                    "to": "Счет 75651667383060284188",
+                },
+                {
+                    "id": 895315941,
+                    "state": "EXECUTED",
+                    "date": "2018-08-19T04:27:37.904916",
+                    "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод с карты на карту",
+                    "from": "Visa Classic 6831982476737658",
+                    "to": "Visa Platinum 8990922113665229",
+                },
+            ],
+        ),
+        ("EUR", []),
+        (
+            "RUB",
+            [
+                {
+                    "id": 873106923,
+                    "state": "EXECUTED",
+                    "date": "2019-03-23T01:09:46.296404",
+                    "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 44812258784861134719",
+                    "to": "Счет 74489636417521191160",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                    "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+                    "description": "Перевод организации",
+                    "from": "Visa Platinum 1246377376343588",
+                    "to": "Счет 14211924144426031657",
+                },
+            ],
+        ),
+        (" ", []),
+    ],
+)
+def test_filter_by_currency(transactions: list, code: str, usd_transaction: list) -> None:
+    generator = filter_by_currency(transactions, code)
+    assert list(generator) == usd_transaction
 
 
-def test_filter_by_currency(transactions: list, usd_transactions: list) -> None:
-    generator = filter_by_currency(transactions, "USD")
-    assert list(generator) == usd_transactions
-
-
-@pytest.fixture
-def descriptions() -> list:
-    return [
-        "Перевод организации",
-        "Перевод со счета на счет",
-        "Перевод со счета на счет",
-        "Перевод с карты на карту",
-        "Перевод организации",
-    ]
-
-
-def test_transaction_descriptions(transactions: list, descriptions: list) -> None:
+# Тесты для transaction_descriptions
+@pytest.mark.parametrize(
+    "transactions, expected",
+    [
+        ([{"description": "Перевод организации"}], "Перевод организации"),
+        ([{"description": "Перевод со счета на счет"}], "Перевод со счета на счет"),
+        ([{"description": "Перевод с карты на карту"}], "Перевод с карты на карту"),
+    ],
+)
+def test_transaction_descriptions(transactions: list, expected: str) -> None:
     generator = transaction_descriptions(transactions)
-    for description in descriptions:
-        assert next(generator) == description
+    # for description in descriptions:
+    assert next(generator) == expected
 
 
 def test_transaction_descriptions_empty() -> None:
@@ -122,6 +150,7 @@ def test_transaction_descriptions_missing_description() -> None:
         list(transaction_descriptions([{"id": 1}]))
 
 
+# Тесты для card_number_generator
 def test_card_number_generator() -> None:
     generator = card_number_generator(1, 5)
     assert next(generator) == "0000 0000 0000 0001"
